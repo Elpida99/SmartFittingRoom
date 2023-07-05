e5b7ce63e59f5dad43ebab3e3ba76bb96e44c6c1
emakri <elpida.makri@netcompany-intrasoft.com>
2023-07-05 22:31:02 +0300

    DEV: initial data

--- backend/src/main/java/hua/it21735/thesis/db/SaveDummyData.java
+++ backend/src/main/java/hua/it21735/thesis/db/SaveDummyData.java
@@ -8,11 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 @Configuration
 public class SaveDummyData {
@@ -27,94 +23,159 @@ public class SaveDummyData {
     ) {
         return (args) -> {
 
-            //save store address
-//            Address storeAddress = new Address();
-//            storeAddress.setCity("Athens");
-//            storeAddress.setNumber("47");
-//            storeAddress.setStreet("Patision");
-//            storeAddress.setPostCode("10433");
-//            addressDao.save(storeAddress);
-//
-//            //save store2 address
-//            Address storeAddress2 = new Address();
-//            storeAddress2.setCity("Kifisia");
-//            storeAddress2.setNumber("13");
-//            storeAddress2.setStreet("Charilaou Trikoupi");
-//            storeAddress2.setPostCode("14564");
-//            addressDao.save(storeAddress2);
-//
-//            //save store
-//            Store store = new Store();
-//            store.setName("S001");
-//            store.setAddress(storeAddress);
-//            store.setEmail("store@gmail.com");
-//            store.setPhoneNumber("2108000056");
-//            storeDao.save(store);
-//
-//            //save store2
-//            Store store2 = new Store();
-//            store2.setName("S002");
-//            store2.setAddress(storeAddress2);
-//            store2.setEmail("store2@gmail.com");
-//            store2.setPhoneNumber("2108000034");
-//            storeDao.save(store2);
-//
-//            //save customer address
-//            Address address = new Address();
-//            address.setCity("Marousi");
-//            address.setNumber("12");
-//            address.setStreet("Grammou");
-//            address.setPostCode("15124");
-//
-//            addressDao.save(address);
-//
-//            //save customer
-//            Customer customer = new Customer();
-//            customer.setFirstName("Konstantinos");
-//            customer.setLastName("Papadopoulos");
-//            customer.setEmail("papadkon@gmail.com");
-//            customer.setPhoneNumber("6972084380");
-//            customer.setAddress(address);
-//
-//            customerDao.save(customer);
-//
-//            //save colours
-//            Colour black = new Colour("BLACK", "BK");
-//            colorDao.save(black);
-//            Colour blue = new Colour("BLUE", "BL");
-//            colorDao.save(blue);
-//            Colour green = new Colour("GREEN", "GR");
-//            colorDao.save(green);
-//            Colour red = new Colour("RED", "R");
-//            colorDao.save(red);
-//            Colour yellow = new Colour("YELLOW", "Y");
-//            colorDao.save(yellow);
-//            Colour whiteBlue = new Colour("WHITE/BLUE", "WB");
-//            colorDao.save(whiteBlue);
-//            Colour multicolour = new Colour("MULTI-COLOUR", "MULT");
-//            colorDao.save(multicolour);
-//
-//            //save sizes
-//            sizeDao.save(new Size(SizeEnum.XS, "62-65", "86-89", "81-83", TargetConsumer.F));
-//            sizeDao.save(new Size(SizeEnum.S, "67-70", "91-94", "86-88", TargetConsumer.F));
-//            sizeDao.save(new Size(SizeEnum.M, "72-75", "97-99", "91-94", TargetConsumer.F));
-//            sizeDao.save(new Size(SizeEnum.L, "79-83", "103-107", "98-100", TargetConsumer.F));
-//            sizeDao.save(new Size(SizeEnum.XL, "86-91", "111-116", "103-108", TargetConsumer.F));
-//
-//            sizeDao.save(new Size(SizeEnum.XS, "62-65", "66", "89-92", TargetConsumer.M));
-//            sizeDao.save(new Size(SizeEnum.S, "67-70", "71", "94-99", TargetConsumer.M));
-//            sizeDao.save(new Size(SizeEnum.M, "72-75", "74-79", "101-107", TargetConsumer.M));
-//            sizeDao.save(new Size(SizeEnum.L, "79-83", "82-87", "109-115", TargetConsumer.M));
-//            sizeDao.save(new Size(SizeEnum.XL, "86-91", "92-97", "117-122", TargetConsumer.M));
-//
-//            sizeDao.save(new Size(SizeEnum.OS, "-", "-", "-", TargetConsumer.F));
-//            sizeDao.save(new Size(SizeEnum.OS, "-", "-", "-", TargetConsumer.M));
-//
-//            //  save categories
-//            categoryDao.save(new Category("Shirts and Blouses", "SB"));
-//            categoryDao.save(new Category("Jeans", "JN"));
-//            categoryDao.save(new Category("Trousers", "TR"));
-//            categoryDao.save(new Category("Denim", "DEN"));
+            // save store addresses
+            Address storeAddress = new Address("Athens", "47", "Patision", "10433");
+            Address storeAddress2 = new Address("Kifisia", "13", "Charilaou Trikoupi", "14564");
+            Address storeAddress3 = new Address("Athens", "40", "Theodorou Diligianni", "10438");
+            Address storeAddress4 = new Address("Peristeri", "58", "Mistriotou", "12137");
+
+            addressDao.save(storeAddress);
+            addressDao.save(storeAddress2);
+            addressDao.save(storeAddress3);
+            addressDao.save(storeAddress4);
+
+            // save stores
+            Store store = new Store("S001", storeAddress, "store@gmail.com", "2108000056");
+            Store store2 = new Store("S002", storeAddress2, "store2@gmail.com", "2108000057");
+            Store store3 = new Store("S003", storeAddress3, "store3@gmail.com", "2108000058");
+            Store store4 = new Store("S004", storeAddress4, "store4@gmail.com", "2108000059");
+
+            storeDao.save(store);
+            storeDao.save(store2);
+            storeDao.save(store3);
+            storeDao.save(store4);
+
+            // save customer addresses
+            Address customerAddress = new Address("Athens", "14", "Amerikis", "10672");
+            Address customerAddress2 = new Address("Kallithea", "100", "Skra", "17673");
+            Address customerAddress3 = new Address("Chalandri", "17", "Agiou Georgiou", "15234");
+            Address customerAddress4 = new Address("Kifisia", "2", "Kolokotroni", "14562");
+            Address customerAddress5 = new Address("Chalandri", "107", "Agias Paraskevis ", "15234");
+            Address customerAddress6 = new Address("Marousi", "67", "Kritis", "15122");
+            Address customerAddress7 = new Address("Athens", "340", "Acharnon", "11145");
+            Address customerAddress8 = new Address("Nea Ionia", "111", "El. Venizelou", "14231");
+            Address customerAddress9 = new Address("Chalandri", "29", "Kosta Varnali", "15233");
+            Address customerAddress10 = new Address("Athens", "61", "Stournari", "10432");
+            Address customerAddress11 = new Address("Athens", "32", "Kallirrois", "14231");
+            Address customerAddress12 = new Address("Agia Paraskevi", "1", "Konstantinou Paleologou", "15342");
+
+            addressDao.save(customerAddress);
+            addressDao.save(customerAddress2);
+            addressDao.save(customerAddress3);
+            addressDao.save(customerAddress4);
+            addressDao.save(customerAddress5);
+            addressDao.save(customerAddress6);
+            addressDao.save(customerAddress7);
+            addressDao.save(customerAddress8);
+            addressDao.save(customerAddress9);
+            addressDao.save(customerAddress10);
+            addressDao.save(customerAddress11);
+            addressDao.save(customerAddress12);
+
+
+            // save customers
+            Customer customer = new Customer("Konstantina", "Michelakou", randomPhoneNumberGen(), "konmich@gmail.com");
+            customer.setAddress(customerAddress);
+
+            Customer customer2 = new Customer("Eleni", "Michelakou", randomPhoneNumberGen(), "elmich@gmail.com");
+            customer2.setAddress(customerAddress);
+
+            Customer customer3 = new Customer("Emilia", "Stefanidi", randomPhoneNumberGen(), "emstefan@gmail.com");
+            customer3.setAddress(customerAddress2);
+
+            Customer customer4 = new Customer("Charalampos", "Kokkinos", randomPhoneNumberGen(), "charkokkinos@gmail.com");
+            customer4.setAddress(customerAddress3);
+
+            Customer customer5 = new Customer("Nikoleta", "Katsarou", randomPhoneNumberGen(), "nikatsarou@gmail.com");
+            customer5.setAddress(customerAddress4);
+
+            Customer customer6 = new Customer("Vasiliki", "Iordanou", randomPhoneNumberGen(), "vasiord@gmail.com");
+            customer6.setAddress(customerAddress5);
+
+            Customer customer7 = new Customer("Areti", "Antoniou", randomPhoneNumberGen(), "anton.areti@gmail.com");
+            customer7.setAddress(customerAddress6);
+
+            Customer customer8 = new Customer("Takis", "Stephanidis", randomPhoneNumberGen(), "stephanidis_takis@gmail.com");
+            customer8.setAddress(customerAddress7);
+
+            Customer customer9 = new Customer("Sofoklis", "Demetriou", randomPhoneNumberGen(), "sofdemet@gmail.com");
+            customer9.setAddress(customerAddress8);
+
+            Customer customer10 = new Customer("Grigoris", "Kontos", randomPhoneNumberGen(), "gregkontos@gmail.com");
+            customer10.setAddress(customerAddress9);
+
+            Customer customer11 = new Customer("Agni", "Giannopoulou", randomPhoneNumberGen(), "agnigiann@gmail.com");
+            customer11.setAddress(customerAddress10);
+
+            Customer customer12 = new Customer("Nikos", "Georgiou", randomPhoneNumberGen(), "nikos_georgiou@gmail.com");
+            customer12.setAddress(customerAddress11);
+
+            Customer customer13 = new Customer("Konstantinos", "Papageorgiou", randomPhoneNumberGen(), "nikpapageo@gmail.com");
+            customer13.setAddress(customerAddress12);
+
+            customerDao.save(customer);
+            customerDao.save(customer2);
+            customerDao.save(customer3);
+            customerDao.save(customer4);
+            customerDao.save(customer5);
+            customerDao.save(customer6);
+            customerDao.save(customer7);
+            customerDao.save(customer8);
+            customerDao.save(customer9);
+
+            customerDao.save(customer10);
+            customerDao.save(customer11);
+            customerDao.save(customer12);
+            customerDao.save(customer13);
+
+            //save colours
+            Colour black = new Colour("BLACK", "BK");
+            colorDao.save(black);
+            Colour blue = new Colour("BLUE", "BL");
+            colorDao.save(blue);
+            Colour green = new Colour("GREEN", "GR");
+            colorDao.save(green);
+            Colour red = new Colour("RED", "R");
+            colorDao.save(red);
+            Colour yellow = new Colour("YELLOW", "Y");
+            colorDao.save(yellow);
+            Colour whiteBlue = new Colour("WHITE/BLUE", "WB");
+            colorDao.save(whiteBlue);
+            Colour multicolour = new Colour("MULTI-COLOUR", "MULT");
+            colorDao.save(multicolour);
+
+            //  save sizes
+            sizeDao.save(new Size(SizeEnum.XS, "62-65", "86-89", "81-83", TargetConsumer.F));
+            sizeDao.save(new Size(SizeEnum.S, "67-70", "91-94", "86-88", TargetConsumer.F));
+            sizeDao.save(new Size(SizeEnum.M, "72-75", "97-99", "91-94", TargetConsumer.F));
+            sizeDao.save(new Size(SizeEnum.L, "79-83", "103-107", "98-100", TargetConsumer.F));
+            sizeDao.save(new Size(SizeEnum.XL, "86-91", "111-116", "103-108", TargetConsumer.F));
+
+            sizeDao.save(new Size(SizeEnum.XS, "62-65", "66", "89-92", TargetConsumer.M));
+            sizeDao.save(new Size(SizeEnum.S, "67-70", "71", "94-99", TargetConsumer.M));
+            sizeDao.save(new Size(SizeEnum.M, "72-75", "74-79", "101-107", TargetConsumer.M));
+            sizeDao.save(new Size(SizeEnum.L, "79-83", "82-87", "109-115", TargetConsumer.M));
+            sizeDao.save(new Size(SizeEnum.XL, "86-91", "92-97", "117-122", TargetConsumer.M));
+
+            sizeDao.save(new Size(SizeEnum.OS, "-", "-", "-", TargetConsumer.F));
+            sizeDao.save(new Size(SizeEnum.OS, "-", "-", "-", TargetConsumer.M));
+
+            //  save categories
+            Category category = new Category("Shirts and Blouses", "SB");
+            Category category1 = new Category("Jeans", "JN");
+            Category category2 = new Category("Trousers", "TR");
+            Category category3 = new Category("Denim", "DEN");
+            Category category4 = new Category("Accessories", "ACC");
+
+            categoryDao.save(category);
+            categoryDao.save(category1);
+            categoryDao.save(category2);
+            categoryDao.save(category3);
+            categoryDao.save(category4);
+
+            //save garments
+            GarmentDetails garmentDetails = new GarmentDetails(null, SizeEnum.S, black, false, 0);
+            Garment garment = new Garment("", "", 0.0F, category, garmentDetails, TargetConsumer.F);
 ////
 ////            // save a couple of garments
 //            File image3 = new File("D:/THESIS/jeans.jpg");
@@ -340,6 +401,18 @@ public class SaveDummyData {
 ////
 //
         };
+    }
+
+    private static String randomPhoneNumberGen() {
+        Random random = new Random();
+        char[] digits = new char[10];
+        digits[0] = '6';
+        digits[1] = '9';
+        digits[2] = (char) (random.nextInt(9) + '1');
+        for (int i = 3; i < 10; i++) {
+            digits[i] = (char) (random.nextInt(10) + '0');
+        }
+        return String.copyValueOf(digits);
     }
 
 }
